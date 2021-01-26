91f761b6bd6b2186ae2907d90a473c3808ad7570
Martin Unzner <martin.unzner@logmein.com>
2021-01-26 12:49:08 +0100

    add acceptance tests

---
+++ src/Student.ts
@@ -0,0 +1,10 @@
+export class Student {
+  constructor(private name: string,
+    private fives: number,
+    private tens: number,
+    private twenties: number) {}
+}
+
+export function mostMoney(students: Student[]): string {
+  return "";
+}
